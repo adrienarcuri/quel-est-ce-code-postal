65151c1a39023fd1bf9ab74d82213fc84634840f
Adrien <adrien.arcuri.pro@gmail.com>
2021-02-19 12:43:20 +0100

    resolve dependencies error

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter_driver/flutter_driver.dart';
 import 'package:quel_est_ce_code_postal/main.dart' as app;
 
 void main() {
